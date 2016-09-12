8c5095c8d3c90e763d91986ea30728ecb66bfe95
张加胜 <leftjs@foxmail.com>
2016-09-12 22:26:31 +0800

    dist

--- src/config/index.js
+++ src/config/index.js
@@ -2,5 +2,5 @@
  * Created by zhangjiasheng on 7/23/16.
  */
 export default {
-	domain: process.env.NODE_ENV != 'production' ? 'http://localhost:8008/api' : 'http://qs-fullstack.greenicetech.cn/api'
+	domain: process.env.NODE_ENV != 'production' ? 'http://localhost:8008/api' : 'http://www.greenicetech.cn/api'
 }
