ddffc95caea10ab42d990a4644de57c66bebdb10
张加胜 <leftjs@foxmail.com>
2016-10-09 10:28:31 +0800

    去除ReactTransition

--- src/components/front/Main.js
+++ src/components/front/Main.js
@@ -3,22 +3,39 @@
  */
 import React from 'react'
 import Container from './Container'
-import { RouteTransition } from 'react-router-transition';
+// import { RouteTransition } from 'react-router-transition';
+
+// class Main extends React.Component{
+// 	render(){
+// 		return (
+// 		<div>
+// 			<RouteTransition
+// 				pathname={this.props.location.pathname}
+// 				atEnter={{ opacity: 0 }}
+// 				atLeave={{ opacity: 0 }}
+// 				atActive={{ opacity: 1 }}
+// 			>
+// 				<Container>
+// 					{this.props.children}
+// 				</Container>
+// 			</RouteTransition>
+// 		</div>
+//
+// 		)
+// 	}
+// }
+//
+// export default Main
+
+
 
 class Main extends React.Component{
 	render(){
 		return (
 		<div>
-			<RouteTransition
-				pathname={this.props.location.pathname}
-				atEnter={{ opacity: 0 }}
-				atLeave={{ opacity: 0 }}
-				atActive={{ opacity: 1 }}
-			>
-				<Container>
-					{this.props.children}
-				</Container>
-			</RouteTransition>
+			<Container>
+				{this.props.children}
+			</Container>
 		</div>
 
 		)
